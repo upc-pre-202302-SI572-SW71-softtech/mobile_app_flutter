4bc7e3347052810a12d5b2533cfefbf97ef05f69
J_Arturo <73813395+pepper419@users.noreply.github.com>
2023-11-03 23:06:41 -0500

    best weather

--- alpinista/lib/main.dart
+++ alpinista/lib/main.dart
@@ -2,95 +2,6 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-class ClimaScreen extends StatefulWidget {
-  @override
-  _ClimaScreenState createState() => _ClimaScreenState();
-}
-
-class _ClimaScreenState extends State<ClimaScreen> {
-  List<CityWeatherData> cityWeatherData = [];
-  final apiKey = 'c5df7aa6bc367505265274d82813805c';
-  final apiKeyc = '6ea050e80616dec93583899563cd6f1c';
-  final cityNames = ['Cuzco', 'Cerro de Pasco', 'Huancayo', 'Ayacucho'];
-
-  @override
-  void initState() {
-    super.initState();
-    fetchWeatherData();
-  }
-
-  Future<void> fetchWeatherData() async {
-    for (var cityName in cityNames) {
-      final geoResponse = await http.get(
-        Uri.parse('http://api.openweathermap.org/geo/1.0/direct?q=$cityName&limit=1&appid=$apiKey'),
-      );
-
-      if (geoResponse.statusCode == 200) {
-        final List<dynamic> geoData = json.decode(geoResponse.body);
-        if (geoData.isNotEmpty) {
-          final Map<String, dynamic> cityData = geoData[0];
-          final lat = cityData['lat'];
-          final lon = cityData['lon'];
-          final currentTime = DateTime.now().millisecondsSinceEpoch ~/ 1000; // Tiempo actual en formato UNIX
-
-          final historyResponse = await http.get(
-            Uri.parse('https://api.openweathermap.org/data/3.0/onecall/timemachine?lat=$lat&lon=$lon&dt=$currentTime&appid=$apiKeyc'),
-          );
-
-          if (historyResponse.statusCode == 200) {
-            final Map<String, dynamic> data = json.decode(historyResponse.body);
-            final String description = data['current']['weather'][0]['description'];
-            final double temperature = data['current']['temp'] - 273.15; // Temperatura en grados Celsius
-
-            setState(() {
-              cityWeatherData.add(CityWeatherData(cityName, description, temperature));
-            });
-          } else {
-            // Manejar errores
-            print('Error al cargar datos históricos para $cityName');
-          }
-        } else {
-          // Manejar errores
-          print('No se encontraron coordenadas para $cityName');
-        }
-      } else {
-        // Manejar errores
-        print('Error al obtener coordenadas para $cityName');
-      }
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return cityWeatherData.isEmpty
-        ? Center(child: Text('No se encontraron datos climáticos.'))
-        : ListView.builder(
-      itemCount: cityWeatherData.length,
-      itemBuilder: (context, index) {
-        final weatherData = cityWeatherData[index];
-        return ListTile(
-          title: Text(weatherData.name),
-          subtitle: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text('Clima: ${weatherData.description}'),
-              Text('Temperatura: ${weatherData.temperature.toStringAsFixed(1)}°C'),
-            ],
-          ),
-        );
-      },
-    );
-  }
-}
-
-class CityWeatherData {
-  final String name;
-  final String description;
-  final double temperature;
-
-  CityWeatherData(this.name, this.description, this.temperature);
-}
-
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -108,7 +19,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _currentIndex = 0;
+  int _currentIndex = 2; // Cambiado a 2 para que muestre la pantalla de Clima por defecto
 
   final List<Widget> _screens = [
     SaludScreen(),
@@ -165,4 +76,89 @@ class RutasScreen extends StatelessWidget {
       child: Text("Vista de Rutas"),
     );
   }
+}
+
+class ClimaScreen extends StatefulWidget {
+  @override
+  _ClimaScreenState createState() => _ClimaScreenState();
+}
+
+class _ClimaScreenState extends State<ClimaScreen> {
+  List<CityWeatherData> cityWeatherData = [];
+  final apiKey = 'c5df7aa6bc367505265274d82813805c';
+  final coordinates = [
+    {'lat': -13.5170887, 'lon': -71.9785356},
+    {'lat': -12.068098 , 'lon': -75.2100953 },
+    {'lat': -14.0764407, 'lon': -72.7210075},
+    {'lat': -13.1604269 , 'lon':-74.2256973 },
+    {'lat': -16.3988667, 'lon':-71.5369607},
+    {'lat': -11.5985493, 'lon':-76.1931885},
+    {'lat': -11.4199882, 'lon':-75.6877142, },
+
+
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchWeatherData();
+  }
+
+  Future<void> fetchWeatherData() async {
+    for (var coord in coordinates) {
+      final lat = coord['lat'];
+      final lon = coord['lon'];
+
+      final weatherResponse = await http.get(
+        Uri.parse('https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&appid=$apiKey'),
+      );
+
+      if (weatherResponse.statusCode == 200) {
+        final Map<String, dynamic> weatherData = json.decode(weatherResponse.body);
+
+        final String cityName = weatherData['name'];
+        final String description = weatherData['weather'][0]['description'];
+        final double temperature = (weatherData['main']['temp'] - 273.15); // Convert to Celsius
+
+        setState(() {
+          cityWeatherData.add(CityWeatherData(cityName, description, temperature));
+        });
+      } else {
+        // Handle errors
+        print('Error al obtener datos climáticos para lat=$lat, lon=$lon');
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return cityWeatherData.isEmpty
+        ? Center(child: Text('No se encontraron datos climáticos.'))
+        : ListView.builder(
+      itemCount: cityWeatherData.length,
+      itemBuilder: (context, index) {
+        final weatherData = cityWeatherData[index];
+        return Card(
+          child: ListTile(
+            title: Text(weatherData.name),
+            subtitle: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Clima: ${weatherData.description}'),
+                Text('Temperatura: ${weatherData.temperature.toStringAsFixed(1)}°C'),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class CityWeatherData {
+  final String name;
+  final String description;
+  final double temperature;
+
+  CityWeatherData(this.name, this.description, this.temperature);
 }
